567cfde43708e24c20c79c2ec459473a6bd0837f
murtanli <70170580+murtanli@users.noreply.github.com>
2022-10-27 11:03:16 +0300

    Ex_10

--- Ex_10/Script-1.sql
+++ Ex_10/Script-1.sql
@@ -11,7 +11,7 @@ WHERE a.district = 'California';
 
 #УПРАЖНЕНИЕ 5.2
 select f.title
-from film fS
+from film f
 inner join film_actor fa 
 on f.film_id  = fa.film_id
 inner join actor a 
@@ -23,4 +23,36 @@ select a1.address addr1, a2.address addr2, a1.city_id
 from address a1
 inner join address a2
 where a1.city_id = a2.city_id 
-and a1.address_id  <> a2.address_id 
+and a1.address_id  <> a2.address_id;
+
+#Запросы к моей бд
+use vk2;
+#ex 1
+select a.url_photo, p.name
+from avat a
+inner join profile p 
+on a.id = p.id
+where length(p.name) > 5;
+# ex 2
+select p.name,p.lastname,v.url_video,a.url_photo,u.`log`
+from profile p
+inner join video v 
+on p.id = v.id 
+inner join avat a
+on a.id = p.id 
+inner join `user` u 
+on u.id = p.id;
+#ex 3
+select v.url_video,p.name
+from `like` l
+inner join video v 
+on l.video_id  = v.id
+inner join profile p 
+on l.profile_id = p.id
+where v.url_video like '%biz%';
+
+
+
+
+
+
