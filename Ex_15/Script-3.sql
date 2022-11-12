cfbd5597a2683a9d7632a872d033581ada90a8ae
murtanli <70170580+murtanli@users.noreply.github.com>
2022-11-12 10:25:20 +0300

    Ex_15 corrected

--- Ex_15/Script-3.sql
+++ Ex_15/Script-3.sql
@@ -1,15 +1,32 @@
-
+use sakila;
 -- Упражнение 10.1
-SELECT с.name, sum(р.amount) 
+SELECT c.first_name, sum(p.amount) 
 FROM customer c 
 LEFT OUTER JOIN payment p 
 ON c.customer_id = p.customer_id 
-GROUP BY c.name;
+GROUP BY c.first_name ;
 
 -- Упражнение 10.2
-SELECT c.name, sum(р.amount) 
+SELECT c.first_name, sum(p.amount) 
 FROM payment p 
 RIGHT OUTER JOIN customer c 
 ON c.customer_id = p.customer_id 
-GROUP BY c.name;
+GROUP BY c.first_name;
+-- Задание к семестровому проекту 1
+
+use vk2;
+
+select p.name, p.lastname, v.url_video 
+from profile p
+left outer join video v 
+on p.video_id = v.id
+group by p.name;
+
+-- Задание к семестровому проекту 2
+
+select p.name, p.lastname, a.url_photo  
+from profile p
+right outer join avat a 
+on p.video_id = a.id
+group by p.name;
 
